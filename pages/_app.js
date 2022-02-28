45630c9782e1bd11b7ace23e2ccd9fa85596c850
Ori Matityahu <ori.matityahou@gmail.com>
2022-02-28 02:40:28 +0200

    01

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import Navbar from '../components/Navbar'
-import '../styles/globals.css'
+import Navbar from '../components/Navbar';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <Navbar />
-      <Component {...pageProps} />
-    </div>
-  )
+function MyApp({Component, pageProps}) {
+    return (
+        <div>
+            <Navbar/>
+            <Component {...pageProps} />
+        </div>
+    );
 }
 
-export default MyApp
+export default MyApp;
